aceeb6d36cc5a63f0de3e8bcb3da9c2d7e1e1c4b
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-17 15:19:13 +0100

    start: start bonus

---
+++ bonus/bonus.js
@@ -0,0 +1,15 @@
+// workflow
+// 1. creo variabili recuperando contenuto html che mi serve
+// 2. creo evento click con operazioni all'attivazioen dell'evento
+//      - cambio immagine
+
+let lamp = document.getElementById("lamp");
+let btn = document.getElementById("toggleLamp");
+let background = document.getElementById("lighting");
+
+background.style.backgroundColor = "#000000";
+
+btn.addEventListener("click", () => {
+    lamp.innerHTML = "<img src='../img/yellow_lamp.png' alt='lampadina accesa'>";
+    background.style.backgroundColor = "#FFEFD5";
+})
